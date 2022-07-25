055a2cd8eb4cc8d024f1aedbeb1e0c3bc9e66942
Nickpandiscia <npandiscia1@gmail.com>
2022-07-25 14:03:11 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FactoryPattenExercise;
+
+namespace FactoryPatternExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the car store! Are you looking for a new vehicle?");            
+            var personsAnswer = Console.ReadLine();
+
+            if(personsAnswer == "yes")
+            {
+                var myNewCar = VehicleFactory.GetVehicle();
+                myNewCar.Drive();
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that wasn't a valid answer");
+            }
+        }
+    }
+}
---
+++ VehicleFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattenExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle()
+        {           
+            Console.WriteLine("We have a Chevy vehicle or a new Harley in stock. Do you want 4 wheels or 2?");
+            var theAnswer = int.Parse(Console.ReadLine());
+                       
+            switch (theAnswer)
+            {
+                case 4:
+                    return  new Car();                   
+                case 2:
+                    return new Motorcycle();
+                    default:
+                    return null;
+
+            }
+
+        }
+    }
+}
